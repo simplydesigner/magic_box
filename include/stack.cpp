796ba5d4201043065b4ecf4ad3cc4fdcd13aad20
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-10-24 10:58:07 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -139,8 +139,9 @@ inline allocator<T>::allocator(allocator const & other) :
 	size_(other.size_),
 	bitset_(other.bitset_) {
 	for (size_t i = 0; i < other.count_; ++i) {
-		if (bitset_.test(i));
+		if (bitset_.test(i)) {
 		this->construct(this->ptr_ + i, other.ptr_[i]);
+		}
 	}
 }
 
