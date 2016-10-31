8cff15c0723bbd8254ae67ddb320444d9990b530
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-10-31 15:53:10 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -136,12 +136,10 @@ inline allocator<T>::allocator(size_t size) :
 }
 
 template<typename T>
-inline allocator<T>::allocator(allocator const & tmp) : 
+inline allocator<T>::allocator(allocator const & other) : 
 	allocator<T>(tmp.size_) {
 	for (size_t i = 0; i < size_; ++i) {
-		if (bitset_.test(i) == false) {
-			construct(ptr_ + i, tmp.ptr_[i]);
-		}
+		construct(ptr_ + i, other.ptr_[i]);
 	}
 }
 
